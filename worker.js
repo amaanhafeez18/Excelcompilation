e0d8e963605622b2dc86ead2f0d423f80c1702e7
Amaan Hafeez <amanqazi1818@gmail.com>
2024-11-21 20:49:18 -0500

    updates worker

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
+
 importScripts("https://cdn.sheetjs.com/xlsx-0.19.1/package/dist/xlsx.full.min.js");
 
 onmessage = async function(event) {
-  const fileData = event.data;
   const sheetNames = ["Company Details", "Financial Info", "Executive"];
   const combinedSheets = {
     "Company Details": [],
@@ -9,31 +9,37 @@ onmessage = async function(event) {
     "Executive": [],
   };
 
-  const totalFiles = fileData.length;
-  for (let i = 0; i < totalFiles; i++) {
-    const file = fileData[i];
+  const files = event.data;
+
+  const processFile = async (file) => {
     try {
-      const fileBuffer = await file.arrayBuffer();
+      const fileBuffer = await file.buffer;
       const workbook = XLSX.read(fileBuffer, { type: "array" });
-      sheetNames.forEach((sheetName) => {
+
+      sheetNames.forEach(sheetName => {
         const sheet = workbook.Sheets[sheetName];
         if (sheet) {
           const sheetData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
-          if (sheetData.length > 0) {
+          if(sheetData.length) {
             if (combinedSheets[sheetName].length === 0) {
-              combinedSheets[sheetName].push(sheetData[0]); // Add header row
+              combinedSheets[sheetName].push(sheetData[0]);
             }
             const dataRows = sheetData.slice(1).filter(row => row.some(cell => cell !== undefined && cell !== null));
-            combinedSheets[sheetName].push(...dataRows); // Add non-empty data rows
+            combinedSheets[sheetName].push(...dataRows);
           }
         }
       });
-      // Send progress update
-      const percentage = Math.round(((i + 1) / totalFiles) * 100);
-      postMessage({ type: 'progress', message: `Processed ${file.name} (${percentage}%)` });
+
+      postMessage({ type: 'progress', message: `Processed ${file.name}` });
+
     } catch (error) {
       postMessage({ type: 'error', file: file.name, error: error.message });
     }
+  };
+
+  // Process each file in sequence
+  for (let i = 0; i < files.length; i++) {
+    await processFile(files[i]);
   }
   
   postMessage({ type: 'complete', combinedSheets });
